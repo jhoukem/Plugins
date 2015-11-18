6d3badd01f1b5bb7e86a04e1031f8a3dd3fa1db5
Ouwe Missi Oukem Jean-hugo <ouwemissiouk@a13p11.fil.univ-lille1.fr>
2015-11-18 15:47:30 +0100

    first

---
+++ src/PluginFilter.java
@@ -0,0 +1,32 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FilenameFilter;
+
+
+public class PluginFilter implements FilenameFilter, ActionListener {
+
+	public boolean accept(File dir, String name) {
+		if(name.lastIndexOf('.')>0)
+		{
+			// get last index for '.' char
+			int lastIndex = name.lastIndexOf('.');
+
+			// get extension
+			String str = name.substring(lastIndex);
+
+			// match path name extension
+			if(str.equals(".class"))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
---
+++ src/PluginFinder.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+
+public class PluginFinder {
+
+	PluginFilter pf = new PluginFilter();
+	File dPath;
+
+	public PluginFinder(File f) {
+		this.dPath = f;
+	}
+	
+	File [] listClassFile(){
+		
+		return null;
+	}
+	
+
+}
